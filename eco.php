f14b158eb61b28d0ca33cc33ecd2d23b1aa4cf86
PHClaus <phhpro@gmail.com>
2016-12-15 17:41:08 +0100

    Update eco.php

--- eco.php
+++ eco.php
@@ -1,112 +1,129 @@
 <?php
-//** host, page, index, data, maximum chars, IP
-$eco_host = 'example.com';
-$eco_page = $_SERVER['SCRIPT_NAME'];
-$eco_indx = str_replace('index.php', '', $eco_page);
-$eco_data = '/home/www/public_html' . $eco_page . '_comments.html';
+/*
+ * host and domain on which the script is running
+ * current page for which comment is received
+ * strip default index from URL
+ * comments data file
+ * maximum characters allowed for comments text
+ * mail account to receive notifications
+ * try to link user's IP
+ */
+$eco_hdom = "example.com";
+$eco_page = $_SERVER["SCRIPT_NAME"];
+$eco_indx = str_replace("index.php", "", $eco_page);
+$eco_data = $_SERVER["DOCUMENT_ROOT"] . $eco_page . "_comments.html";
 $eco_tmax = 1024;
-$eco_myip = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+$eco_nota = "info";
+$eco_myip = gethostbyaddr($_SERVER["REMOTE_ADDR"]);
 
-//** default user name, admin prefix and suffix
-$eco_user = 'anonymous';
-$eco_apfx = 'secret_';
-$eco_asfx = 'root';
+/*
+ * default user name
+ * admin prefix
+ * admin suffix
+*/
+$eco_user = "anonymous";
+$eco_apfx = "foo";
+$eco_asfx = "bar";
 
-//** init name, status, and save flag
-$eco_name = '';
-$eco_stat = '';
-$eco_save = '';
+/*
+ * send notification for new comments
+ * mailto token
+ * header token
+ */
+$eco_note = "y";
+$eco_mail = "$eco_nota@$eco_hdom";
+$eco_head = "From: Easy Comments <$eco_mail>";
 
-//** notify flag, mailto and from
-$eco_note = 'n';
-$eco_mail = 'info@example.com';
-$eco_from = 'From: Comment ' . $eco_host . ' <info@' . $eco_host . '>';
-
-//** captcha
+//** init captcha
 $eco_cmin = 1;
 $eco_cmax = 9;
 $eco_cone = mt_rand($eco_cmin, $eco_cmax);
 $eco_ctwo = mt_rand($eco_cmin, $eco_cmax);
 
-//** version
-$eco_ver  = 20161027;
+//** init internals -- must be empty!
+$eco_name = "";
+$eco_stat = "";
+$eco_save = "";
+
+//** script version
+$eco_ver  = 20161215;
 
 //** redirect helper
 function redir($url) {
 
   if (!headers_sent()) {    
-    header('Location: ' . $url);
+    header("Location: $url");
   } else {
-    echo '<meta http-equiv="refresh" content="0; url=' . $url . '">';
+    echo "<meta http-equiv=\"refresh\" content=\"0; url=$url\">";
   }
 }
 
 //** form submitted
-if (isset ($_POST['eco_post'])) {
-  //** filter name, text, and hard links, fix escaped quotes
-  $eco_name = htmlspecialchars($_POST['eco_name']);
-  $eco_text = htmlspecialchars($_POST['eco_text']);
+if (isset ($_POST["eco_post"])) {
+  //** filter name and text
+  $eco_name = htmlspecialchars($_POST["eco_name"]);
+  $eco_text = htmlspecialchars($_POST["eco_text"]);
 
-  //** strip anchors
-  $eco_text = preg_replace('/<a([\s\S])*a>/', '***', $_POST['eco_text']);
+  //** bust anchors
+  $eco_text = preg_replace("/<a([\s\S])*a>/", "***", $_POST["eco_text"]);
   $eco_text = str_replace("\'", "'", $eco_text);
 
   //** link captcha
-  $eco_csum = $_POST['eco_csum'];
-  $eco_cone = $_POST['eco_cone'];
-  $eco_ctwo = $_POST['eco_ctwo'];
+  $eco_csum = $_POST["eco_csum"];
+  $eco_cone = $_POST["eco_cone"];
+  $eco_ctwo = $_POST["eco_ctwo"];
   $eco_cval = $eco_cone + $eco_ctwo;
 
   //** substitute missing name
-  if ($eco_name == '') {
+  if ($eco_name == "") {
     $eco_name = $eco_user;
   }
 
   //** check restricted name
   if (($eco_name == $eco_asfx) || 
-      ($eco_name == 'admin') || 
-      ($eco_name == 'administrator') || 
-      ($eco_name == 'root') || 
-      ($eco_name == 'webmaster')) {
+      ($eco_name == "admin") || 
+      ($eco_name == "administrator") || 
+      ($eco_name == "root") || 
+      ($eco_name == "webmaster")) {
     $eco_name = $eco_user;
-    $eco_stat = 'That name is restricted!';
-    $eco_save = 'n';
+    $eco_stat = "Sorry, that name is restricted!";
+    $eco_save = "n";
   }
 
-  //** append user key to user post or admin reply
+  //** append identifier to user post or admin reply
   if ($eco_name == $eco_apfx . $eco_asfx) {
     $eco_name = $eco_asfx;
-    $eco_ukey = '#';
+    $eco_ukey = "#";
   } else {
     $eco_name = $eco_name;
-    $eco_ukey = '$';
+    $eco_ukey = "$";
   }
 
   //** check missing text
-  if ($eco_text == '') {
-    $eco_stat = 'Text field cannot be empty!';
-    $eco_save = 'n';
+  if ($eco_text == "") {
+    $eco_stat = "Cannot post empty comment!";
+    $eco_save = "n";
   }
 
-  //** check maximum characters
+  //** check and trim maximum characters
   if (strlen($eco_text) > $eco_tmax) {
     $eco_clen = strlen($eco_text);
     $eco_cfix = ($eco_clen - $eco_tmax);
-    $eco_stat = 'Maximum characters allowed: ' . $eco_tmax . ' (' . $eco_cfix . ' characters have been removed!)';
+    $eco_stat = "Maximum characters allowed: $eco_tmax ($eco_cfix characters have been removed!)";
     $eco_text = substr($eco_text, 0, $eco_tmax);
   }
 
   //** check captcha and re-generate
   if ($eco_cval != $eco_csum) {
-    $eco_stat = 'Invalid verification code!';
-    $eco_save = 'n';
+    $eco_stat = "Invalid verification code!";
+    $eco_save = "n";
     $eco_cone = mt_rand($eco_cmin, $eco_cmax);
     $eco_ctwo = mt_rand($eco_cmin, $eco_cmax);
   }
 
   //** valid comment
-  if ($eco_save != 'n') {
-    $eco_post = '      <div id="eco_' . gmdate('Y_m_d_H_i_s') . '_' . $eco_myip . '_' . $eco_name . '" class="eco_item"><span>' . gmdate('Y-m-d H:i:s') . ' ' . $eco_name . ' ' . $eco_ukey . '</span> ' . $eco_text . "</div>\n";
+  if ($eco_save != "n") {
+    $eco_post = '      <div id="eco_' . gmdate('Ymd_His') . '_' . $eco_myip . '_' . $eco_name . '" class="eco_item"><span>' . gmdate('Y-m-d H:i:s') . ' ' . $eco_name . ' ' . $eco_ukey . '</span> ' . $eco_text . "</div>\n";
 
     //** save comment to existing data file
     if (is_file($eco_data)) {
@@ -119,22 +136,18 @@ if (isset ($_POST['eco_post'])) {
     //** check if user post
     if ($eco_name != $eco_asfx) {
 
-      //** check whether to send notification -- NO VISUAL
-      if ($eco_note == 'y') {
+      //** check whether to send notification
+      if ($eco_note == "y") {
 
         //** prepare message
-        $eco_subj = $eco_host . '_Comment';
-        $eco_text = $eco_name . ' regarding ' . $eco_host . $eco_indx . "\n\n" . $eco_text;
+        $eco_subj = "New_Comment";
+        $eco_text = $eco_name . " regarding " . $eco_hdom . $eco_indx . "\n\n" . $eco_text;
 
-        // try sending
-        if (mail($eco_mail, $eco_subj, $eco_text, $eco_from)) {
-          $eco_info = 'Thank you, your message has been sent.';
-        } else {
-          $eco_info = 'There was a problem sending your message!';
-        }
+        // try sending -- NO VISUAL
+        mail($eco_mail, $eco_subj, $eco_text, $eco_head);
 
-        //** try to prevent re-submission
-        redir($eco_indx . '#Comments');
+        //** try to catch re-submission
+        redir($eco_indx . "#Comments");
       }
     }
   }
@@ -148,7 +161,7 @@ if (!isset ($eco_this)) {
   //** print header depending whether data file exists or not
   if (is_file ($eco_data)) {
 ?>
-      <p id="eco_main"><a href="<?php echo $eco_indx; ?>#Add_Comment" title="Add new comment">Add Comment</a></p>
+      <p id="eco_main"><a href="<?php echo $eco_indx; ?>#Add_Comment" title="Click here to add new comment">Add Comment</a></p>
 <?php
     //** include existing data file
     if (is_file ($eco_data)) {
@@ -171,7 +184,7 @@ if (!isset ($eco_this)) {
         <label for="eco_text">Text (<small>maximum <span id="eco_ccnt"><?php echo $eco_tmax; ?></span> characters</small>)</label>
       </p>
       <div>
-        <textarea name="eco_text" id="eco_text" rows="2" cols="26" title="Please enter the text of your comment" class="input" onFocus="eco_tmax('eco_text', 'eco_ccnt', <?php echo $eco_tmax; ?>)" onKeyDown="eco_tmax('eco_text', 'eco_ccnt', <?php echo $eco_tmax; ?>)" onKeyUp="eco_tmax('eco_text', 'eco_ccnt', <?php echo $eco_tmax; ?>)"></textarea>
+        <textarea name="eco_text" id="eco_text" rows="4" cols="26" title="Please enter the text of your comment" class="input" onFocus="eco_tmax('eco_text', 'eco_ccnt', <?php echo $eco_tmax; ?>)" onKeyDown="eco_tmax('eco_text', 'eco_ccnt', <?php echo $eco_tmax; ?>)" onKeyUp="eco_tmax('eco_text', 'eco_ccnt', <?php echo $eco_tmax; ?>)"></textarea>
       </div>
       <p>
         <label for="eco_csum">Code</label> 
