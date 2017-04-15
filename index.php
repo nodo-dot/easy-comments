b3b0c7f9e43674c0fc1d53b9f659b78b3ebe30c6
PHClaus <phhpro@gmail.com>
2017-04-15 11:03:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,10 @@
  */
 
 
-//** start session -- comment out if parent page has already set this
-//session_start();
-
 /*
  * default directory index
- * comments data file
- * comments log file
+ * comments data file -- not used with manual approval
+ * comments log file -- not used whit manual approval
  */
 $eco_dirx = "index.php";
 $eco_cdat = "_comments.html";
@@ -20,30 +17,32 @@ $eco_clog = "/eco/log.html";
 
 /*
  * maximum characters allowed for comments text
- * accept latin characters only (n y)
+ * accept latin characters only (y n)
  * default anonymous user name
  */
 $eco_tmax = 1024;
-$eco_lato = "n";
+$eco_lato = "y";
 $eco_anon = "anonymous";
 
 /*
- * send notification for new comments (n y)
- * mail account to get notified -- assuming same domain -- must exist
+ * send notifications for new comments (y n)
+ * mail account to receive notifications
+ * manual approval of new posts (y n) -- enabled requires notifications
  */
-$eco_note = "n";
-$eco_nota = "info";
+$eco_note = "y";
+$eco_mail = "info@" . $_SERVER['HTTP_HOST'];
+$eco_mapp = "y";
 
 /*
  * admin prefix
  * admin suffix
  */
-$eco_apfx = "your_token_here";
+$eco_apfx = "YOUR_ROOT_PREFIX";
 $eco_asfx = "root";
 
 /*
  * restricted names plus typos -- use your illusion
- * all lowercase here -- script takes care of user case
+ * all lowercase here -- user case handled by script
  */
 $eco_rest = array (
                     "administrator",
@@ -60,14 +59,13 @@ $eco_rest = array (
                   );
 
 /*
- * query string token to list log file
+ * query string to list log file
+ * delay between posts in seconds -- 0 to disable
  * date and time format
  */
-$eco_list = "your_token_here";
-$eco_date = gmdate('Y-m-d H:i:s');
-
-//** delay between posts in seconds -- set to 0 to disable
+$eco_list = "YOUR_LIST_TOKEN";
 $eco_tdel = 60;
+$eco_date = gmdate('Y-m-d H:i:s');
 
 
 /*
@@ -79,22 +77,17 @@ $eco_tdel = 60;
 
 /*
  * host on which the script is running
- * current page for which comment is received
+ * current page to which the comment applies
  * strip default index
  * comments data file
  * try to link user IP
+ * mail header
  */
 $eco_host = $_SERVER['HTTP_HOST'];
 $eco_page = $_SERVER['PHP_SELF'];
 $eco_indx = str_replace($eco_dirx, "", $eco_page);
 $eco_data = $_SERVER['DOCUMENT_ROOT'] . $eco_page . $eco_cdat;
 $eco_myip = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-
-/*
- * mailto address
- * mail header
- */
-$eco_mail = "$eco_nota@$eco_host";
 $eco_head = "From: Easy Comments <$eco_mail>";
 
 /*
@@ -120,7 +113,7 @@ $eco_stat = "";
 $eco_save = "";
 
 //** script version
-$eco_make = 20170409;
+$eco_make = 20170415;
 
 //** redirect helper
 function eco_post($url) {
@@ -137,7 +130,7 @@ function eco_post($url) {
 $eco_prot = "http";
 
 //** check https
-if ((isset ($_SERVER['HTTPS'])) && ("on" === $_SERVER['HTTPS'])) {
+if (isset ($_SERVER['HTTPS']) && "on" === $_SERVER['HTTPS']) {
   $eco_prot .= "s";
 }
 
@@ -160,6 +153,26 @@ if ($_SERVER['QUERY_STRING'] == $eco_list) {
   }
 }
 
+// check session status helper
+function is_session_started() {
+
+  if (php_sapi_name() !== "cli") {
+
+    if (version_compare(phpversion(), "5.4.0", ">=")) {
+      return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
+    } else {
+      return session_id() === "" ? FALSE : TRUE;
+    }
+  }
+
+  return FALSE;
+}
+
+//** check session status
+if (is_session_started() === FALSE) {
+  session_start();
+}
+
 //** link session
 $_SESSION['eco_tbeg'] = time();
 $eco_tbeg = $_SESSION['eco_tbeg'];
@@ -170,7 +183,7 @@ if (isset ($_POST["eco_post"])) {
   $eco_name = htmlspecialchars($_POST['eco_name']);
   $eco_text = htmlspecialchars($_POST['eco_text']);
 
-  //** basic links filter
+  //** filter links
   if (preg_match("/(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))/", $eco_text)) {
     $eco_stat = "Text must not contain links!";
     $eco_save = "n";
@@ -183,7 +196,7 @@ if (isset ($_POST["eco_post"])) {
   $eco_cval = $eco_cone + $eco_ctwo;
 
   //** substitute anon if name is empty or spaces only
-  if (($eco_name == "") || (preg_match("/^\s*$/", $eco_name))) {
+  if ($eco_name == "" || preg_match("/^\s*$/", $eco_name)) {
     $eco_name = $eco_anon;
   }
 
@@ -229,7 +242,7 @@ if (isset ($_POST["eco_post"])) {
     $eco_latx = "/[^\\p{Common}\\p{Latin}]/u";
 
     //** check name and text
-    if ((preg_match($eco_latx, $eco_name)) || (preg_match($eco_latx, $eco_text))) {
+    if (preg_match($eco_latx, $eco_name) || preg_match($eco_latx, $eco_text)) {
       $eco_stat = "Only latin characters allowed!";
       $eco_save = "n";
     }
@@ -257,49 +270,59 @@ if (isset ($_POST["eco_post"])) {
     //** build comments entry
     $eco_post = '      <div id="eco_' . gmdate('Ymd_His_') . $eco_myip . '_' . $eco_name . '" class="eco_item"><span>' . $eco_date . " " . $eco_name . " " . $eco_ukey . "</span> " . $eco_text . "</div>\n";
 
-    //** check existing data file
-    if (is_file($eco_data)) {
-      $eco_post .= file_get_contents($eco_data);
-    }
+    //** prepare message
+    $eco_subj = "New_Comment";
+    $eco_text = $eco_name . " regarding " . $eco_prot . $eco_host . $eco_indx . "\n\n" . $eco_post;
 
-    //** update data file
-    file_put_contents($eco_data, $eco_post);
+    //** check moderator flag
+    if ($eco_mapp != "y") {
 
-    //** link log file and build log entry
-    $eco_clog = $_SERVER['DOCUMENT_ROOT'] . $eco_clog;
-    $eco_ulog = '<div>' . $eco_date . ' <a href="' . $eco_prot . $eco_host . $eco_indx . '" title="Click here to open">' . $eco_host . $eco_indx . "</a></div>\n";
-
-    //** check existing log file
-    if (is_file($eco_clog)) {
-      $eco_ulog .= file_get_contents($eco_clog);
-    }
-
-    //** update log file
-    file_put_contents($eco_clog, $eco_ulog);
-
-    //** check if user post
-    if ($eco_name != $eco_asfx) {
-
-      //** check notification flag
-      if ($eco_note == "y") {
-
-        //** prepare message
-        $eco_subj = "New_Comment";
-        $eco_text = $eco_name . " regarding " . $eco_host . $eco_indx . "\n\n" . $eco_text;
-
-        // try sending -- NO VISUAL
-        mail($eco_mail, $eco_subj, $eco_text, $eco_head);
+      //** check existing data file
+      if (is_file($eco_data)) {
+        $eco_post .= file_get_contents($eco_data);
       }
+
+      //** update data file
+      file_put_contents($eco_data, $eco_post);
+
+      //** link log file and build log entry
+      $eco_clog = $_SERVER['DOCUMENT_ROOT'] . $eco_clog;
+      $eco_ulog = '<div>' . $eco_date . ' <a href="' . $eco_prot . $eco_host . $eco_indx . '" title="Click here to open">' . $eco_host . $eco_indx . "</a></div>\n";
+
+      //** check existing log file
+      if (is_file($eco_clog)) {
+        $eco_ulog .= file_get_contents($eco_clog);
+      }
+
+      //** update log file
+      file_put_contents($eco_clog, $eco_ulog);
+
+      //** check if user post
+      if ($eco_name != $eco_asfx) {
+
+        //** check notification flag and send mail
+        if ($eco_note == "y") {
+          mail($eco_mail, $eco_subj, $eco_text, $eco_head);
+        }
+      }
+
+      //** link timer session
+      $_SESSION['eco_tfrm'] = htmlspecialchars($_POST['eco_tbeg']);
+
+      //** try to catch re-submission
+      eco_post($eco_prot . $eco_host . $eco_indx . "#Comments");
+    } else {
+      //** send message
+      mail($eco_mail, $eco_subj, $eco_text, $eco_head);
+
+      //** link timer session
+      $_SESSION['eco_tfrm'] = htmlspecialchars($_POST['eco_tbeg']);
+
+      //** try to catch re-submission
+      eco_post($eco_prot . $eco_host . $eco_indx . "#Comments");
     }
-
-    //** link timer session
-    $_SESSION['eco_tfrm'] = htmlspecialchars($_POST['eco_tbeg']);
-
-    //** try to catch re-submission
-    eco_post($eco_prot . $eco_host . $eco_indx . "#Comments");
-
   } else {
-    //** avoid clearing valid fields
+    //** avoid clearing existing input
     $eco_name = $eco_name;
     $eco_text = $eco_text;
   }
@@ -307,24 +330,32 @@ if (isset ($_POST["eco_post"])) {
 
 //** check if comments enabled
 if (!isset ($eco_this)) {
+
+  //** check if moderator is on but notifications are off
+  if ($eco_mapp == "y" && $eco_note == "n") {
+?>
+    <p id="eco_stat">Easy Comments Error</p>
+    <p>The moderator flag is set but notifications are disabled! Please edit the script's configuration to enable notifications.</p>
+<?php
+  } else {
 ?>
     <form action="<?php echo $eco_prot . $eco_host . $eco_indx; ?>#Comments" method="POST" id="Comments">
       <div id="eco_stat"><?php echo $eco_stat; ?></div>
 <?php
-  //** print header depending whether data file exists or not
-  if (is_file($eco_data)) {
+    //** print header depending whether data file exists or not
+    if (is_file($eco_data)) {
 ?>
       <p id="eco_main"><a href="<?php echo $eco_prot . $eco_host . $eco_indx; ?>#Add_Comment" title="Click here to add new comment">Add Comment</a></p>
 <?php
-    //** include existing data file
-    if (is_file($eco_data)) {
-      include ($eco_data);
-    }
-  } else {
+      //** include existing data file
+      if (is_file($eco_data)) {
+        include ($eco_data);
+      }
+    } else {
 ?>
       <p id="eco_main">No comments yet. Be the first to share your thoughts.</p>
 <?php
-  }
+    }
 ?>
       <p id="Add_Comment">
         <label for="eco_name">Name</label> <span class="eco_by">(A-Z only)</span>
@@ -349,18 +380,26 @@ if (!isset ($eco_this)) {
       </p>
       <p>
 <?php
-  //** link timer difference
-  $eco_tdif = ($eco_tbeg - $_SESSION['eco_tfrm']);
+    //** link timer difference
+    $eco_tdif = ($eco_tbeg - $_SESSION['eco_tfrm']);
 
-  //** check timer status
-  if ($eco_tdif > $eco_tdel) {
-    echo '        <input name="eco_post" type="submit" value="Add Comment" title="Click here to post your comment" class="input">' . "\n";
-  } else {
-    echo "        Please wait " . ($eco_tdel - $eco_tdif) . " seconds before posting again!<br />\n        Refresh this page tp update the timer status.\n";
-  }
+    //** check timer status
+    if ($eco_tdif > $eco_tdel) {
+      echo '        <input name="eco_post" type="submit" value="Add Comment" title="Click here to post your comment" class="input">' . "\n";
+    } else {
+      echo "        Please wait " . ($eco_tdel - $eco_tdif) . " seconds before posting again!<br />\n        Refresh this page to update the timer status.\n";
+    }
 ?>
       </p>
-      <p class="eco_by">All posts are monitored and subject to removal!</p>
+<?php
+    //** check moderator flag
+    if ($eco_mapp == "y") {
+      $eco_mtxt = "New posts will be listed upon approval.";
+    } else {
+      $eco_mtxt = "All posts are monitored and subject to removal.";
+    }
+?>
+      <p class="eco_by"><?php echo $eco_mtxt; ?></p>
       <p class="eco_by"><a href="http://phclaus.com/php-scripts/easy-comments/" title="Click here to get your own free copy of PHP Easy Comments">Powered by PHP Easy Comments v<?php echo $eco_make; ?></a></p>
     </form>
     <script type="text/javascript">
@@ -378,5 +417,6 @@ if (!isset ($eco_this)) {
     }
     </script>
 <?php
+  }
 }
 ?>
