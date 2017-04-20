d777cff545c6f043ece468074e055434a9d5419b
PHClaus <phhpro@gmail.com>
2017-04-20 19:43:28 +0200

    Create restricted.php

---
+++ restricted.php
@@ -0,0 +1,14 @@
+<?php
+//** check restricted names
+if (
+    preg_match("/admin/i", $eco_name) ||
+    preg_match("/local/i", $eco_name) ||
+    preg_match("/moder/i", $eco_name) ||
+    preg_match("/root/i", $eco_name) ||
+    preg_match("/test/i", $eco_name) ||
+    preg_match("/webma/i", $eco_name)
+   ) {
+  $eco_name = $eco_anon;
+  $eco_stat = "Sorry, at least part of the selected name is restricted!";
+  $eco_save = "n";
+}
