b3bc44e733313e36cb0125d472f7a9259818cf26
PHClaus <phhpro@gmail.com>
2017-05-05 13:56:19 +0200

    Update restricted.php

--- restricted.php
+++ restricted.php
@@ -5,18 +5,26 @@
  * admin => admin, ADMINfoo, AdminisTraitor...
  * webma => webma, myWebMama, yourWebMASTER...
  */
-if (preg_match("/admin/i", $eco_name) ||
-    preg_match("/local/i", $eco_name) ||
-    preg_match("/moder/i", $eco_name) ||
-    preg_match("/root/i", $eco_name) ||
-    preg_match("/test/i", $eco_name) ||
-    preg_match("/webma/i", $eco_name)) {
 
-  //** check if admin post
-  if ($eco_name === $eco_apfx . $eco_asfx) {
-    $eco_name = $eco_apfx . $eco_asfx;
-  } else {
-    $eco_name = $eco_anon;
-    $eco_stat = "Part or all of selected name is restricted!";
+$eco_aray = array (
+                  "admin",
+                  "local",
+                  "moder",
+                  "root",
+                  "test",
+                  "webma",
+                 );
+
+foreach ($eco_aray as $eco_nono) {
+
+  if (preg_match("/$eco_nono/i", $eco_name)) {
+
+    //** check if admin post
+    if ($eco_name === $eco_apfx . $eco_asfx) {
+      $eco_name = $eco_apfx . $eco_asfx;
+    } else {
+      $eco_name = $eco_anon;
+      $eco_stat = "Part or all of the selected name is restricted!";
+    }
   }
 }
