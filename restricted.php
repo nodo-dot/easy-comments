fc28c4e7b5305a86175f68b06870f4827a66763b
PHClaus <phhpro@gmail.com>
2017-05-05 11:35:59 +0200

    Update restricted.php

--- restricted.php
+++ restricted.php
@@ -3,19 +3,17 @@
  * check restricted names -- meaningful fragments are ok
  *
  * admin => admin, ADMINfoo, AdminisTraitor...
- * webma => webma, YourWebMama, MyWebMASTER...
+ * webma => webma, myWebMama, yourWebMASTER...
  */
-if (
-    preg_match("/admin/i", $eco_name) ||
+if (preg_match("/admin/i", $eco_name) ||
     preg_match("/local/i", $eco_name) ||
     preg_match("/moder/i", $eco_name) ||
     preg_match("/root/i", $eco_name) ||
     preg_match("/test/i", $eco_name) ||
-    preg_match("/webma/i", $eco_name)
-   ) {
+    preg_match("/webma/i", $eco_name)) {
 
   //** check if admin post
-  if ($eco_name == $eco_apfx . $eco_asfx) {
+  if ($eco_name === $eco_apfx . $eco_asfx) {
     $eco_name = $eco_apfx . $eco_asfx;
   } else {
     $eco_name = $eco_anon;
