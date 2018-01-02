f5199218583292132c90f48e90c54ec4de505436
PHClaus <15097539+phhpro@users.noreply.github.com>
2018-01-02 05:16:25 +0100

    Add files via upload

---
+++ lang/es-ES.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * PHP Easy Comments is a trivial comments script with minimal bloat.
+ *
+ * @category PHP_Chat_Scripts
+ * @package  PHP_Atom_Chat
+ * @author   P H Claus <phhpro@gmail.com>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @link     https://github.com/phhpro/easy-comments
+ *
+ * Language file es-ES for Spanish Spanish
+ *
+ * Please translate only the value of Locale_String.
+ * Do not translate any Reference_ID!
+ *
+ * Always use a copy of the en-GB language file as a template.
+ */
+
+
+//** Init array
+$eco_lang = array();
+
+//** Strings
+$eco_lang['miss_log']     = "¡El archivo log no existe!";
+$eco_lang['mod_flag']     = "¡El modo moderador está activo pero las notificaciones están deshabilitadas!";
+$eco_lang['chk_settings'] = "Por favor verifique su configuración para corregir el error.";
+$eco_lang['fix_error']    = "La secuencia de comandos se desactivará hasta que se solucione el error.";
+$eco_lang['invalid_char'] = "¡Nombre contiene caracteres inválidos!";
+$eco_lang['invalid_code'] = "¡Código de control inválido!";
+$eco_lang['miss_text']    = "No puedo publicar comentarios vacíos!";
+$eco_lang['only_latin']   = "¡Solo caracteres latinos permitidos!";
+$eco_lang['view_item']    = "Haga clic aquí para ver este elemento";
+$eco_lang['mod_wait']     = "Gracias. Tu comentario está esperando ser moderado.";
+$eco_lang['add_new']      = "Haga clic aquí para agregar un nuevo comentario";
+$eco_lang['add_comment']  = "Agregar comentario";
+$eco_lang['be_first']     = "Sin comentarios aún. Se el primero en compartir tus pensamientos.";
+$eco_lang['name']         = "Nombre";
+$eco_lang['az']           = "A - Z";
+$eco_lang['only']         = "solamente";
+$eco_lang['type_name']    = "Escriba aquí para ingresar su nombre o déjelo en blanco para publicar anónimo";
+$eco_lang['text']         = "Texto";
+$eco_lang['type_text']    = "Escribe aquí para ingresar el texto de tu comentario";
+$eco_lang['code']         = "Codigo";
+$eco_lang['type_code']    = "Escriba aquí para ingresar el código de control";
+$eco_lang['post']         = "Enviar";
+$eco_lang['type_post']    = "Haga clic aquí para publicar su comentario";
+$eco_lang['post_again']   = "Gracias. Puedes publicar de nuevo en";
+$eco_lang['seconds']      = "segundos";
+$eco_lang['man_refresh']  = "Actualiza esta página para actualizar manualmente el temporizador.";
+$eco_lang['mod_require']  = "Las publicaciones nuevas requieren la aprobación del moderador";
+$eco_lang['be_polite']    = "Sé educado y evita el mal lenguaje";
+$eco_lang['get_copy']     = "Haga clic aquí para obtener una copia gratuita de";
+$eco_lang['powered_by']   = "Energizado por";
+$eco_lang['char_rem']     = "caracteres restantes";
+$eco_lang['name_rest']    = "¡Parte o todo el nombre seleccionado está restringido!";
