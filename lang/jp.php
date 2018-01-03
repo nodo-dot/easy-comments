24ad2a3feba915466190373bbac3b3f1cc3ea50f
PHClaus <15097539+phhpro@users.noreply.github.com>
2018-01-03 21:47:52 +0100

    20180103.5

---
+++ lang/jp.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * @category  PHP_Comment_Scripts
+ * @package   PHP_Easy_Comments
+ * @author    P H Claus <phhpro@gmail.com>
+ * @copyright 2015 - 2018 P H Claus
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @link      https://github.com/phhpro/easy-comments
+ *
+ * Language file for Japanese
+ */
+
+
+//** Init array
+$eco_lang = array();
+
+//** Strings
+$eco_lang['miss_log']     = "ログファイルがありません！";
+$eco_lang['mod_flag']     = "モデレーターモードはアクティブですが、通知は無効になっています！";
+$eco_lang['chk_settings'] = "設定を確認してエラーを修正してください。";
+$eco_lang['fix_error']    = "スクリプトは、エラーが修正されるまで無効になります。";
+$eco_lang['invalid_code'] = "コントロールコードが無効です！";
+$eco_lang['miss_text']    = "空のコメントを投稿できません！";
+$eco_lang['only_latin']   = "ラテン文字のみが許されます！";
+$eco_lang['view_item']    = "このアイテムを表示するにはここをクリック";
+$eco_lang['add_new']      = "新しいコメントを追加するにはここをクリック";
+$eco_lang['add_comment']  = "コメントを追加";
+$eco_lang['be_first']     = "コメントはまだありません。あなたの考えを共有してください。";
+$eco_lang['name']         = "名";
+$eco_lang['type_name']    = "あなたの名前を入力するかここに入力するか、匿名を投稿するために空欄にしてください";
+$eco_lang['text']         = "テキスト";
+$eco_lang['type_text']    = "あなたのコメントのテキストを入力するにはここに入力してください";
+$eco_lang['code']         = "コード";
+$eco_lang['type_code']    = "コントロールコードを入力するにはここに入力してください";
+$eco_lang['post']         = "役職";
+$eco_lang['type_post']    = "あなたのコメントを投稿するにはここをクリック";
+$eco_lang['post_again']   = "ありがとうございました。あなたは再び投稿することができます";
+$eco_lang['seconds']      = "秒";
+$eco_lang['man_refresh']  = "このページをリフレッシュして、タイマーを手動で更新します。";
+$eco_lang['mod_require']  = "新しい投稿にはモデレータの承認が必要";
+$eco_lang['be_polite']    = "礼儀正しく、悪い言葉を控える";
+$eco_lang['get_copy']     = "このスクリプトの無料コピーを入手するには、ここをクリックしてください";
+$eco_lang['powered_by']   = "搭載";
+$eco_lang['char_rem']     = "残りの文字";
+$eco_lang['restricted']   = "選択した名前の一部または全部が制限されています。";
---
+++ lang/pt.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * @category  PHP_Comment_Scripts
+ * @package   PHP_Easy_Comments
+ * @author    P H Claus <phhpro@gmail.com>
+ * @copyright 2015 - 2018 P H Claus
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @link      https://github.com/phhpro/easy-comments
+ *
+ * Language file for Portuguese
+ */
+
+
+//** Init array
+$eco_lang = array();
+
+//** Strings
+$eco_lang['miss_log']     = "Ficheiro de registro perdido!";
+$eco_lang['mod_flag']     = "O modo Moderador está ativo, mas as notificações estão desabilitadas!";
+$eco_lang['chk_settings'] = "Verifique as configurações para corrigir o erro.";
+$eco_lang['fix_error']    = "O script será desativado até o erro ser corrigido.";
+$eco_lang['invalid_code'] = "Código de controle inválido!";
+$eco_lang['miss_text']    = "Não é possível publicar um comentário vazio!";
+$eco_lang['only_latin']   = "Somente caracteres latinos são permitidos!";
+$eco_lang['view_item']    = "Clique aqui para ver este item";
+$eco_lang['add_new']      = "Clique aqui para adicionar um novo comentário";
+$eco_lang['add_comment']  = "Adicionar comentário";
+$eco_lang['be_first']     = "Ainda não há comentários. Seja o primeiro à compartilhar seus pensamentos.";
+$eco_lang['name']         = "Nome";
+$eco_lang['type_name']    = "Digite aqui para inserir seu nome ou deixar em branco para postar anônimo";
+$eco_lang['text']         = "Texto";
+$eco_lang['type_text']    = "Digite aqui para inserir o texto do seu comentário";
+$eco_lang['code']         = "Código";
+$eco_lang['type_code']    = "Digite aqui para inserir o código de controle";
+$eco_lang['post']         = "Postar";
+$eco_lang['type_post']    = "Clique aqui para postar seu comentário";
+$eco_lang['post_again']   = "Obrigado. Você pode postar novamente em";
+$eco_lang['seconds']      = "segundos";
+$eco_lang['man_refresh']  = "Atualize esta página para atualizar manualmente o temporizador.";
+$eco_lang['mod_require']  = "Novas postagens exigem aprovação do moderador";
+$eco_lang['be_polite']    = "Seja educado e abstenha-se de um idioma ruim";
+$eco_lang['get_copy']     = "Clique aqui para obter uma cópia gratuita deste script";
+$eco_lang['powered_by']   = "Distribuído por";
+$eco_lang['char_rem']     = "caracteres restantes";
+$eco_lang['restricted']   = "Parte ou todo o nome selecionado é restrito!";
